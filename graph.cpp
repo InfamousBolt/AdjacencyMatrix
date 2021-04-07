3634c82ab5f76015016c5fe42cf33ec96feb45d4
InfamousBolt <47386839+InfamousBolt@users.noreply.github.com>
2021-04-07 16:41:43 +0530

    Add files via upload

---
+++ graph.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+
+class Graph{
+    int nodes;
+    bool** adjMat;
+
+public:
+    Graph(int nodes){
+        this->nodes=nodes;
+        adjMat= new bool*[nodes];
+        for(int i=0;i<nodes;i++){
+            adjMat[i]=new bool[nodes];
+            for(int j=0;j<nodes;j++){
+                adjMat[i][j]=false;
+            }
+        }
+    }
+
+    void addEdge(int u,int v){
+        adjMat[u][v]=true;
+        adjMat[v][u]=true;
+    }
+
+    void printMat(){
+        for(int i=0;i<nodes;i++){
+            cout<<i<<": ";
+            for(int j=0;j<nodes;j++){
+                cout<<adjMat[i][j]<<" ";
+            }
+            cout<<"\n";
+
+        }
+    }
+};
+
+int main(){
+    int n,e;
+    cin>>n>>e;
+    Graph graph(n);
+    for(int i=0;i<e;i++){
+        int u,v;
+        cin>>u>>v;
+        graph.addEdge(u,v);
+    }
+    graph.printMat();
+    return 0;
+}
